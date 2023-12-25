971eae43e3b469811b65434aadcd0b9bdb2bd993
Asim Rzaev <146167300+Asim25th@users.noreply.github.com>
2023-12-25 10:50:04 +0300

    Update Bus.cs

--- Bus.cs
+++ Bus.cs
@@ -1,4 +1,4 @@
-﻿namespace TrucksAndBuses
+namespace TrucksAndBuses
 {
     class Bus : Auto
     {
@@ -19,24 +19,13 @@
             Console.WriteLine($"\nСПРАВКА: При весе от 1000 кг  скорость становится в 1,5 раза меньше, \nа при весе от 2000 кг - в 2 раза меньше. Средний вес человека - {avgWeight} кг");
             Console.Write("Введите количество пассажиров (до 50): ");
             people = Convert.ToInt32(Console.ReadLine());
-
             while (people > 50)
             {
                 Console.Write("ОШИБКА: Введено некорректное значение. Попробуйте ещё раз\nВведите количество пассажиров (до 50): ");
                 people = Convert.ToInt32(Console.ReadLine());
             }
             weight = people * avgWeight;
-        }
 
-        protected override void Output()
-        {
-            Console.Write($"\nВес: {Math.Round(weight, 3)} кг");
-            base.Output();
-            stop();
-        }
-
-        protected override void Move()
-        {
             if (weight >= 2000)
             {
                 speed = Math.Round(speed / 2, 2);
@@ -45,6 +34,7 @@
             {
                 speed = Math.Round(speed / 1.5, 2);
             }
+
             if (speed <= 40)
             {
                 consumption = Math.Round(consumption * 2, 2);
@@ -53,10 +43,39 @@
             {
                 consumption = Math.Round(consumption * 1.5, 2);
             }
-            base.Move();
         }
 
-        private void stop() // остановки
+        protected override void Output()
+        {
+            Console.Write($"\nВес: {Math.Round(weight, 3)} кг");
+            base.Output();
+            Stop();
+        }
+
+        //protected override void Move()
+        //{
+        //    if (weight >= 2000)
+        //    {
+        //        speed = Math.Round(speed / 2, 2);
+        //    }
+        //    else if (weight >= 1000)
+        //    {
+        //        speed = Math.Round(speed / 1.5, 2);
+        //    }
+
+        //    if (speed <= 40)
+        //    {
+        //        consumption = Math.Round(consumption * 2, 2);
+        //    }
+        //    else if (speed > 90)
+        //    {
+        //        consumption = Math.Round(consumption * 1.5, 2);
+        //    }
+
+        //    base.Move();
+        //}
+
+        private void Stop() // остановки
         {
             int cost = 40;
             cost *= people;
