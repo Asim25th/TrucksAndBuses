b0a47c635d799b83f695ac7c60bb8b9177861798
Asim Rzaev <146167300+Asim25th@users.noreply.github.com>
2023-12-25 10:03:23 +0300

    Update Truck.cs

--- Truck.cs
+++ Truck.cs
@@ -21,22 +21,6 @@
                 Console.Write("ОШИБКА: Введено некорректное значение. Попробуйте ещё раз\nВведите вес груза (до 5000 кг): ");
                 weight = Convert.ToDouble(Console.ReadLine());
             }
-            if (weight >= 4000)
-            {
-                speed = Math.Round(speed / 2, 2);
-            }
-            else if (weight >= 2500)
-            {
-                speed = Math.Round(speed / 1.5, 2);
-            }
-            if (speed <= 40)
-            {
-                consumption = Math.Round(consumption * 2, 2);
-            }
-            else if (speed > 90)
-            {
-                consumption = Math.Round(consumption * 1.5, 2);
-            }
         }
 
         protected override void Output()
